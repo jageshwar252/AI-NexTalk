3d57148a07e3722757cba85d5a4474a15ba11b7f
jageshwar252 <jageshwarkumar252@gmail.com>
2026-02-18 18:07:55 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://ai-nextalk-backend.onrender.com",
 });
 
 // Add a request interceptor to always use the latest token
